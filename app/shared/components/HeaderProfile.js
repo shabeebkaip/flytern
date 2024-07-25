61cd5e3bda466bd9ffd1db7ea04c1618a0f1dbc6
mohammed mishal <michum515@gmail.com>
2024-07-25 17:47:08 +0530

    logout icon isuues fixed

--- app/shared/components/HeaderProfile.js
+++ app/shared/components/HeaderProfile.js
@@ -111,7 +111,7 @@ const HeaderProfile = () => {
                                 onClick={userLogout}
                             >
                              
-                                    <Image width={24} height={24} className='w-6 h-6' src="/Logout.png" alt="" />
+                                    <Image  className='w-6 h-6' src="/Logout.png" alt="" width={300} height={300} />
                              
                                 {selectedLanguageAndCountry?.language?.code === "ar" ? 'تسجيل خروج' : 'Logout'}
                             </div>
