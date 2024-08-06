1be2b9918833fcf853c3c17658f7ffc4383b30d5
mohammed mishal <michum515@gmail.com>
2024-08-06 12:07:56 +0530

    forgot password completed

--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -2,12 +2,15 @@
 import React from 'react'
 import StoreProvider from '../StoreProvider'
 import Reset from './contents/Reset'
+import { SnackbarProvider } from 'notistack'
 
 const page = () => {
   return (
     <div>
         <StoreProvider>
+          <SnackbarProvider>
             <Reset/>
+          </SnackbarProvider>
         </StoreProvider>
     </div>
   )
