a36ca2434ac2f6f34f6e99f727090579747870af
mohammed mishal <michum515@gmail.com>
2024-08-13 10:31:03 +0530

    select button working in mobile my bookings

--- app/my-bookings/components/MobFlightBookingCard.jsx
+++ app/my-bookings/components/MobFlightBookingCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 import { useAppSelector } from '@/lib/hooks'
+import { encryptId } from '@/lib/utils'
 
 const MobFlightBookingCard = ({ flight }) => {
     const { selectedLanguageAndCountry } = useAppSelector(state => state.sharedState)
+    const encryptedBookingRef = encryptId(flight.bookingRef)
+
+    console.log(flight,'kkk');
+    
+
     return (
         <div className='flex flex-col gap-5 p-5 bg-white rounded-lg '>
             <div className='flex items-center justify-between gap-3 '>
                 <div>
-                    <Image className='h-5 ' src={flight.airlineImgUrl} alt="" width={300} height={300} />
+                    <Image className='h-5 w-20 object-contain ' src={flight.airlineImgUrl} alt="" width={300} height={300} />
                 </div>
                 <div className='flex items-center gap-2'>
                     <div className={`px-2 h-7 text-[10px]  ${flight.refund_status ? 'bg-tag-color text-tag-color' : 'bg-red-600 bg-opacity-20 text-red-600'} rounded-md  font-medium flex justify-center items-center`}>
@@ -42,10 +48,32 @@ const MobFlightBookingCard = ({ flight }) => {
 
             </div>
             <div className='flex items-center justify-between'>
-                <h3 className='text-xs font-medium text-orange-400 '>AED <span className='text-xl font-semibold text-orange-400'>{parseFloat(flight.price).toFixed(3)}</span> </h3>
-                <button className='h-10 px-6 py-1.5 bg-emerald-800 rounded-md justify-center items-center gap-1 inline-flex text-center text-white text-sm font-medium'>
-                    {selectedLanguageAndCountry?.language?.code === "ar" ? arabic_translation.select : 'Select'}
-                </button>
+                <h3 className='text-xs font-medium text-orange-400 '>AED <span className='text-xl font-semibold text-orange-400'>{parseFloat(flight.paidAmount).toFixed(3)}</span> </h3>
+                {
+                        flight.status === "ISSUED" ?
+                            <button
+                                className='h-10 px-6 py-1.5 bg-emerald-800 rounded-md justify-center items-center gap-1 inline-flex text-center text-white text-sm font-medium'
+                                onClick={() => {
+                                    if (typeof window !== 'undefined') {
+                                        window.location.href = `/payment-summary/?mode=view&ref=${encryptedBookingRef}`;
+                                    }
+                                }}
+                            >
+                                {selectedLanguageAndCountry?.language?.code === "ar" ? "عرض الحجز" : "View Booking"}
+                            </button>
+                            : flight.status === "PENDING" ?
+                                <button
+                                    className='h-10 px-6 py-1.5 bg-emerald-800 rounded-md justify-center items-center gap-1 inline-flex text-center text-white text-sm font-medium'
+                                    onClick={() => {
+                                        if (typeof window !== 'undefined') {
+                                            window.location.href = `/payment-method/?ref=${encryptedBookingRef}`;
+                                        }
+                                    }}
+                                >
+                                    {selectedLanguageAndCountry?.language?.code === "ar" ? "ادفع الآن" : "Pay Now"}
+                                </button>
+                                : null
+                    }
             </div>
         </div>
     )
